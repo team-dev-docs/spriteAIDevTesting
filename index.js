1afe75acd268a31a376be28d8505b3e7b715996c
andrew.ryan.vanbeek@gmail.com <andrewvanbeek@andrews-MacBook-Air-2.local>
2024-02-08 10:36:33 -0800

    adds some stuff

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ async function getUniqueColors(imagePath, options = {}) {
 
 export const sprite = {
     async generateSprite(description, options = {}) {
-      //check again plesase!! does this work
+      //check again plesase!! does this work ok nice
       if(options.iterations) {
         let i = 0; 
         let iterations = []

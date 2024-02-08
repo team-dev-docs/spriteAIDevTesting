1e9078efdd96b662c48753ce66c3c7691adde27e
andrew.ryan.vanbeek@gmail.com <andrewvanbeek@andrews-MacBook-Air-2.local>
2024-02-08 09:39:28 -0800

    adds some new stuff

--- index.js
+++ index.js
@@ -64,6 +64,89 @@ async function getUniqueColors(imagePath, options = {}) {
 
 export const sprite = {
     async generateSprite(description, options = {}) {
+      if(options.iterations) {
+        let i = 0; 
+        let iterations = []
+        while (i < options.iterations) {
+          const openAiObject = new OpenAI();
+          const dalle3 = openAiObject.images
+          const response = await dalle3.generate({
+              model: "dall-e-3",
+              prompt: `I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS.  
+              Generate 6 frames of a 24-bit character of the requested character of ${description}, optimized for walking animations.
+              Other Instructions:
+              
+              -The top half of the image should be the frames and the bottom half should be a blank white background with nothing in it.
+              -Style should resemble Super Nintendo graphics.
+              -The background of the image, and frame should just be the color white, with no extra items, lines, text, or grids.
+              -The frames should be two rows with 3 columns each, so a 2 by 3 table.
+              `,
+              n: 1,
+              size: options?.size || "1024x1024",
+            });
+            console.log(response)
+  
+    
+          //   console.log("this is the result", result.choices)
+            console.log(response.data[0])
+            const res = await axios.get(response.data[0].url, { responseType: 'arraybuffer' });
+            const imgBuffer = Buffer.from(res.data)
+          //   const colorsArray = await getUniqueColors(imgBuffer)
+          //   console.log(colorsArray)
+          if(options?.save) {
+              const currentWorkingDirectory = process.cwd()
+              let pictureName = description.replace(/\s+/g, '');
+              let pictureFilename = `${currentWorkingDirectory}${path.sep}assets${path.sep}${pictureName}.png`
+              await sharp(imgBuffer).ensureAlpha().greyscale().toFile(pictureFilename)
+          }
+          let grayImageBuffer = await sharp(imgBuffer).ensureAlpha().greyscale().toBuffer()
+          let base64Image = grayImageBuffer.toString('base64');
+  
+          // If you need a data URL, you can prepend the appropriate prefix
+          let imageDataUrl = `data:image/jpeg;base64,${base64Image}`;
+          // const base64Image = encodeImage("./test.png");
+          const result = await openAiObject.chat.completions.create({
+              model: "gpt-4-vision-preview",
+              max_tokens: 1000,
+              messages: [
+                {
+                  role: "user",
+                  content: [
+                    { type: "text", text: `For this 1024x1024 image, what would be the frameWidth and frameHeight if I was to use this image as a spritesheet for this phaser js function:
+                    
+                    this.load.spritesheet('test', path to png, { frameWidth: 115, frameHeight: 380 });
+                    ` 
+                  },
+                    {
+                      type: "image_url",
+                      image_url: imageDataUrl,
+                    },
+                  ],
+                },
+              ],
+            });
+            let framesResponse = result[0]
+            const jsonFrameResponse = await openAiObject.chat.completions.create({
+              model: "gpt-3.5-turbo-1106",
+              response_format: { type: "json_object" },
+              max_tokens: 1000,
+              messages: [
+                {
+                  role: "user",
+                  content: [
+                    { type: "text", text: `Return a JSON with best the frameHeight and Framewidth from this description: ${result.choices[0].message}` },
+                  ],
+                },
+              ],
+            });
+  
+  
+     
+            iterations.push({messages: jsonFrameResponse.choices[0].message, image: imageDataUrl})
+
+        }
+        return iterations
+      } else {
         const openAiObject = new OpenAI();
         const dalle3 = openAiObject.images
         const response = await dalle3.generate({
@@ -139,6 +222,8 @@ export const sprite = {
 
    
           return {messages: jsonFrameResponse.choices[0].message, image: imageDataUrl}
+      }
+      
         //   image_url = response.data.data[0].url;
         //   return image_url
     },

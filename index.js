714fd93040353be68daab6f4ec595dcdc5301bc2
avb-dev-docs <123575048+avb-dev-docs@users.noreply.github.com>
2024-03-15 09:45:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ async function getUniqueColors(imagePath, options = {}) {
 
 export const sprite = {
     async generateSprite(description, options = {}) {
-//this is a test!!!!!!!!
+
       if(options.iterations) {
         let i = 0; 
         let iterations = [] 

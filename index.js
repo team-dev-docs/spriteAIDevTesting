b29f404842814bc384f774e688da9b15a0d276fc
tim-dev-docs <tim@dev-docs.io>
2024-08-23 10:47:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import path from "path";
 
 async function removeBackgroundColor(inputPath, outputPath, targetColor, colorThreshold = 0, options = {}) {
   //yeah so coolllllldiosjfgiodjsf
-  //kdsoijfiodjsiofdsfdsfsadaskjhuiguifd
+  //kdsoijfiodjsiofdsfdsfsafd
   const image = await Jimp.read(inputPath);
 
     // Define the color you want to replace (e.g., white) or even blue, or yellow!

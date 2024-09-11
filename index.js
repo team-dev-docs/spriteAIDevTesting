54a4b0d3c6bdff673d96e92666b629f35274212e
avb-is-me <104213687+avb-is-me@users.noreply.github.com>
2024-09-11 13:45:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import fs from "fs"
 import path from "path";
 
 async function removeBackgroundColor(inputPath, outputPath, targetColor, colorThreshold = 0, options = {}) {
-  //yeah so coolllllll!huygtlkojihugytijhuiyftydrtfgdgfdgfdgsaisjaidjsia
+  //yeah so coolllllll!huygtlkojihugytijhuiyftydrtfgdgfdgfdgsaisjaidjsiasadsf
   // fdsafewqfewqfcdsafdsafdsafcdssasaafdgfgdsafdsooodsfoidjsiofjdiosfdsfdsf
   const image = await Jimp.read(inputPath);
 

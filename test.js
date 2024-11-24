2adc0d8a116e67a7aa591778ba94ca50f3780b33
avb-is-me <104213687+avb-is-me@users.noreply.github.com>
2024-11-24 10:55:32 -0800

    Update test.js

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ import { sprite }  from "./index.js";
 
 
 const main = async function() {
+    //whatever
     const result = await sprite.generateSprite("a robot samurai cat", {save: true})
     console.log('this is the finished result o yeah', result)
 }
